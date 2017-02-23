1587556be810d8f531df2cb73ca062fad3509498
Tyler Lucas <tyblu@live.com>
2017-02-23 16:23:23 -0700

    Modified to output data to serial, and slowed down to allow for more accurate multimeter measurements during operation.

--- SIK Circuits/Circuit_11_modified/Circuit_11_modified.ino
+++ SIK Circuits/Circuit_11_modified/Circuit_11_modified.ino
@@ -1,24 +1,28 @@
 const int buzzerPin = 9;
-
-// The tempo is how fast to play the song. To make the song play faster, decrease this value.
 const int tempo = 150;
-
 
 void setup() 
 {
   pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 
 void loop() 
 {
-  int i;
-  for (i = 1; i < 88; i++) {
-    tone(buzzerPin, (int)( 440 * pow( (float)2, (float)(i-49)/(float)12 ) ), tempo*4);
-    delay(tempo*4);       // wait for tone to finish
+  int keyNum, freq;
+  char str[18];
+  
+  for (keyNum = 1; keyNum < 88; keyNum++) {
+    freq = (int)( 440 * pow( (float)2, (float)(keyNum-49)/(float)12 ) );
+    tone(buzzerPin, freq, tempo*10);
+    delay(tempo*10);       // wait for tone to finish
+    
+    sprintf(str, "key %02d  freq %05d", keyNum, freq);
+    Serial.println(str);
   }
   
   delay(1000);
   
-  //while(true){}
+  while(true){}
 }
