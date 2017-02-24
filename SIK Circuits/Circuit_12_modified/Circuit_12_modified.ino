631834685cfc524c0b9973901fb3551a44a60712
Tyler Lucas <tyblu@live.com>
2017-02-23 21:06:34 -0700

    Cleanup.

--- SIK Circuits/Circuit_12_modified/Circuit_12_modified.ino
+++ SIK Circuits/Circuit_12_modified/Circuit_12_modified.ino
@@ -2,21 +2,15 @@ const int motorPin = 9;
 const int speedLimits[] = {34, 255, 255};    // Motor stall/min speed, start speed, max speed
 const int timeStart = 15;                    // Time to get motor moving from rest with start speed voltage
 
-
-void setup()
-{
+void setup() {
   pinMode(motorPin, OUTPUT);
-
   Serial.begin(9600);
 }
 
-
-void loop()
-{
+void loop() {
   motorAcceleration();
 //     serialSpeed();
 }
-
 
 // Jump-starts the motor and sets it at any speed. 
 void motorGo(int speedGoal) {
@@ -32,31 +26,25 @@ void motorGo(int speedGoal) {
   
   analogWrite(motorPin, speedGoal);      // set speed
   
-  Serial.print("--> Speed set to ");          // write about it
+  Serial.print("--> Speed set to ");     // write about it
   Serial.println(speedGoal);
 }
 
-// This function slowly accelerates the motor to full speed,
-// then back down to zero.
-void motorAcceleration()
-{
+// This function slowly accelerates the motor to full speed, then back down to zero.
+void motorAcceleration() {
   int speed;
   int delayTime = 20; // milliseconds between each speed step
   
   // accelerate the motor
-
-  for(speed = 0; speed <= 255; speed++)
-  {
+  for(speed = 0; speed <= 255; speed++) {
     motorGo(speed);	// set the new speed
-    delay(delayTime);           	// delay between speed steps
+    delay(delayTime);   // delay between speed steps
   }
   
   // decelerate the motor
-
-  for(speed = 255; speed >= 0; speed--)
-  {
+  for(speed = 255; speed >= 0; speed--) {
     motorGo(speed);	// set the new speed
-    delay(delayTime);           	// delay between speed steps
+    delay(delayTime);   // delay between speed steps
   }
 }
 
@@ -67,41 +55,25 @@ void motorAcceleration()
 // type your desired speed into the small text entry bar at the
 // top of the window and click "Send" or press return. The motor
 // will then operate at that speed. The valid range is 0 to 255.
-void serialSpeed()
-{
+void serialSpeed() {
   int speed;
   
   Serial.println("Type a speed (0-255) into the box above,");
   Serial.println("then click [send] or press [return]");
   Serial.println();  // Print a blank line
 
-  // In order to type out the above message only once,
-  // we'll run the rest of this function in an infinite loop:
-
-  while(true)  // "true" is always true, so this will loop forever.
-  {
+  while(true) {  // loop forever
     // First we check to see if incoming data is available:
-  
-    while (Serial.available() > 0)
-    {
+    while (Serial.available() > 0) {
       // If it is, we'll use parseInt() to pull out any numbers:
-      
       speed = Serial.parseInt();
-  
-      // Because analogWrite() only works with numbers from
-      // 0 to 255, we'll be sure the input is in that range:
-  
       speed = constrain(speed, 0, 255);
       
-      // We'll print out a message to let you know that the
-      // number was received:
-      
+      // We'll print out a message to let you know that the number was received:
       Serial.print("Setting speed to ");
       Serial.println(speed);
   
-      // And finally, we'll set the speed of the motor!
-      
-      motorGo(speed);
+      motorGo(speed);  // And finally, we'll set the speed of the motor!
     }
   }
 }
