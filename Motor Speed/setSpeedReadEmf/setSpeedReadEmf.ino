f116c8ef4d5e3c134829bb7901413bf87701b7d4
Tyler Lucas <tyblu@live.com>
2017-04-28 21:57:45 -0600

    About to change the circuit again. It works as it is, using two transistors to control PWM and ON/OFF at the motor base, but I'm going to get rid of one transistor and just measure a whole bunch of node voltages to more easily calculate emf.

--- Motor Speed/setSpeedReadEmf/setSpeedReadEmf.ino
+++ Motor Speed/setSpeedReadEmf/setSpeedReadEmf.ino
@@ -13,24 +13,26 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const int motor_pin = 9; // conected to transistor base to drive motor
-const int emf_pin = A0;  // analog input related to motor back-emf
+const int motor_pin = 9; // PWM driving NPN to control motor
+const int emf_pin = A0;  // back-emf related input
+const int pause_pin = 8; // digital driving NPN to pause motor
 
 void setup() {
-  pinMode(motor_pin, OUTPUT);  // set up the pin as an OUTPUT
+  pinMode( motor_pin, OUTPUT );
+  pinMode( pause_pin, OUTPUT );
 
   lcd.begin(16, 2); //Initialize the 16x2 LCD
   lcd.clear();  // clear any old data displayed on the LCD
   lcd.print("Hello, COMP444!");
   lcd.setCursor(0,1);
-  lcd.print("emf to RPM v2.0");
+  lcd.print("emf to RPM v2.1");
   delay(1500);
 }
 
 void loop() {
 
-  int t_pause = 500;    // pause time in microseconds
-  unsigned long t_now, t_then;
+  unsigned long t_pause = 500;  // pause time in [us], ulong for quick add to micros()
+  unsigned long t_limit;
 
   int motor_speed_target_last = 0;
   int motor_speed_target = 0;
@@ -40,40 +42,38 @@ void loop() {
   int motor_speed_target_to_current = 0;
   int motor_speed_target_to_current_abs = 0;
 
-  int adc_current = 0;
-  int adc_history[100] = {0};
+  int adc_values[100] = {0};
   
   while ( true ) {
 
-    motor_speed_target = 181;
+    digitalWrite( pause_pin, HIGH );  // connect to common
+
+    motor_speed_target = map(; // 1V-3V rating/5V supply=20-60% duty=51-153pwm
     while ( motor_speed_target != motor_speed_current )
     {   // ramp motor speed to target
       
       motor_speed_target_to_current = motor_speed_target - motor_speed_current;
       motor_speed_target_to_current_abs = abs( motor_speed_target_to_current );
-      motor_speed_steps = ( motor_speed_target_to_current_abs + 10 )/10; // most sig. digit+1
-      motor_speed_next = motor_speed_current + motor_speed_target_to_current / motor_speed_steps;
+//      motor_speed_steps = ( motor_speed_target_to_current_abs + 10 )/10; // most sig. digit+1
+//      motor_speed_next = motor_speed_current + motor_speed_target_to_current / motor_speed_steps;
+      motor_speed_next = motor_speed_current + motor_speed_target_to_current / motor_speed_target_to_current_abs; // +/-1 at a time
   
       analogWrite( motor_pin, motor_speed_next );
-      delay(100);
+//      delay(200); // slow ramp
+      delay(50);  // lower delay with only single increment changes
   
       motor_speed_current = motor_speed_next;
     }
-  
-//    analogWrite( motor_pin, LOW );
-//    delayMicroseconds( 100 );   // allow emf to stabilize
-    adc_current = analogRead( emf_pin );
-//    analogWrite( motor_pin, motor_speed_current );
 
-    push_to_vector( adc_history, adc_current );
+//    // pause motor current, measure emf, reconnect driver
+//    digitalWrite( pause_pin, LOW );   // turn off motor driver
+//    delayMicroseconds( 250 );   // allow emf to stabilize
+//    t_limit = micros() + t_pause; // set time limit for measurements
+//    int n = 0;
+//    while ( micros() < t_limit ) {
+//      adc_values[n++] = analogRead( emf_pin ); // takes ~100us
+//      if ( n > 98 ) { break; } // if this happens, there's a timer issue
+//    }
+//    digitalWrite( pause_pin, HIGH );  // turn on motor driver
   }
 }
-
-void push_to_vector ( int (&vec)[100], int value ) {
-  int n = 100-1;
-  while ( n-- > 1 ){
-    vec[n] = vec[n-1];  // shuffle down
-  }
-  vec[0] = value;       // push to top
-}
-
