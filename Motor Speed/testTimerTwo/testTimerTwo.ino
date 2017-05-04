9cf534bb971614078b2a8c74abf6786f3d13826a
Tyler Lucas <tyblu@live.com>
2017-05-04 13:09:15 -0600

    Improved accuracy of timing with micros() for no good reason. Now you can watch the LED blink a few times during the test as it loops through several times instead of just once. Discovered that asm("") is not optimized away by the compiler, so putting it inside of otherwise empty loops forces them to stay put. Useful when measuring empty loop time.

--- Motor Speed/testTimerTwo/testTimerTwo.ino
+++ Motor Speed/testTimerTwo/testTimerTwo.ino
@@ -19,10 +19,12 @@
 /* Change these to experiment. Oscilloscope can less than 1 microsecond,
  * humans can see over 67000 microseconds (15 Hz). */
 #ifdef SERIAL
-#define TIMER_DELAY 300000      // slow down LED blink so you can see it
-const int loop_delay_time = 5;  // in [seconds]
+#define TIMER_DELAY 500000        // slow down LED blink so you can see it
+#define TIMER_DELAY_TEST_LOOPS 10 // test time will be TIMER_DELAY_TEST_LOOPS * TIMER_DELAY
+const int loop_delay_time = 5;    // in [seconds]
 #else
 #define TIMER_DELAY 220
+#define TIMER_DELAY_TEST_LOOPS 1
 #endif
 
 // Pin assignments.
@@ -82,25 +84,54 @@ void loop()
   Serial.print("  counter_limit: "); Serial.println(counter_limit);
 #endif
 
+  unsigned int prev_state = toggle_me_B;
+  
   timer2_start();
 
 #ifdef SERIAL
-  unsigned long timestamp;
+  unsigned long timestamp, timestamp_offset;
+
+  /* This attempts to remove the timing error introduced by overhead
+   *  in the loop checking for the toggled bit.
+   */
+  prev_state = toggle_me_A;
+  
+  int n; // removed from for loop to get outside of micros() timer
+  
+  timestamp = micros();
+  for ( n=1; n<2; n++ )
+  {
+    while ( toggle_me_A == prev_state ) { test_while_loop(); asm(""); }
+    asm("");
+  }
+  timestamp_offset = micros() - timestamp;
+  
+  // undo changes from this test
+  counter--; 
+  digitalWrite( output_pin_A, toggle_me_A^=1 );
+  prev_state = toggle_me_B;
+  Serial.print("\nTimestamp offset: "); Serial.print(timestamp_offset); Serial.println(" [us] per loop.\n");
 #endif
 
-  unsigned int prev_state = toggle_me_B;
 
   while ( true ) {
 #ifdef SERIAL
     Serial.println("\nTimer2 enabled!");
 
+    int n; // removed from for loop to get outside of micros() timer
+
     timer2_restart();
     timestamp = micros();
     timer2_enable();
 
-    while ( toggle_me_B == prev_state ) { }
-
-    timestamp = micros() - timestamp;
+    for ( n=0; n<TIMER_DELAY_TEST_LOOPS; n++ )
+    {
+      while ( toggle_me_B == prev_state ) { }
+      prev_state = toggle_me_B;
+    }
+    
+    timestamp = micros() - timestamp - TIMER_DELAY_TEST_LOOPS * timestamp_offset;
+    
     timer2_disable();
     timer2_stop();
 //    timer2_restart();
@@ -109,11 +140,11 @@ void loop()
 
     Serial.print("Timer2 disabled!");
     Serial.print(" Time: ");
-    Serial.print( float(timestamp/1e6), 3 );
+    Serial.print( float(timestamp/1e6), 4 );
     Serial.print(" seconds. That is ");
-    if ( timestamp > TIMER_DELAY ) { Serial.print( "+" ); }
-    Serial.print( 100*( (float)(timestamp) / (float)(TIMER_DELAY) )-100, 2 );
-    Serial.println("%% from the target delay.");
+    if ( timestamp > TIMER_DELAY_TEST_LOOPS * TIMER_DELAY ) { Serial.print( "+" ); }
+    Serial.print( 100*( (float)(timestamp) / (float)(TIMER_DELAY_TEST_LOOPS * TIMER_DELAY) )-100, 3 );
+    Serial.println("% from the target delay.");
     Serial.print("Timer restarting in "); Serial.print(loop_delay_time); Serial.println(" seconds...");
     
     delay( loop_delay_time*1000 );
@@ -175,7 +206,7 @@ void timer2_set_period( long microseconds )
 }
 
 
-void timer2_start() // don't call before timer2_init
+void timer2_start() // don't call before timer2_init; disables ISR, call timer2_enable after
 {
   unsigned int tcnt2_temp;
 
@@ -204,24 +235,25 @@ void timer2_stop()    // stop timer, ISR not disabled (but won't fire)
 }
 
 
-void timer2_restart() // starts clock; does not zero timer, use timer2_start for that
+void timer2_restart() // starts clock; does not zero timer, use timer2_restart_zero for that
 {
   TCCR2B |= timer2_prescale_bits;
 }
 
-void timer2_restart_zero()
-{
-  unsigned int tcnt2_temp;
 
-  TIMSK2 &= ~(1 << TOIE2);  // Timer2 OVF ISR disable
+void timer2_restart_zero()  // resets timer back to tcnt2, starts clock
+{
+//  TIMSK2 &= ~(1 << TOIE2);  // Timer2 OVF ISR disable
   
   oldSREG = SREG;
   cli();
   TCNT2 = tcnt2;    // restart counter
   SREG = oldSREG;
   timer2_restart(); // prescale determined earlier with timer2_init or timer2_set_period
-  // may be phantom interrupt here, you may want to clear ISR flag
+  
+  // may be phantom interrupt here, clearing flag instead of waiting for timer tick
   TIFR2 = 0xFF;             // clear interrupt flag
+  
 //  TIMSK2 |= (1 << TOIE2);   // Timer2 OVF ISR enable
 }
 
@@ -242,17 +274,24 @@ void timer2_disable() // disables ISR, clears interrupt flag
 
 // the test ISR
 ISR(TIMER2_OVF_vect) {
-  TCNT2 = tcnt2;    // restart timer
+  timer2_restart_zero();  // restart and zero timer
   counter++;
-  toggle_me_A ^= 1;    // toggles between 0 and 1 (LOW or HIGH), breaks for other numbers
-  digitalWrite( output_pin_A, toggle_me_A );
-  if ( counter > counter_limit )
+//  toggle_me_A ^= 1;    // toggles between 0 and 1 (LOW or HIGH), breaks for other numbers
+  digitalWrite( output_pin_A, toggle_me_A^=1 );
+  if ( counter+1 >= counter_limit )
   {
-    toggle_me_B ^= 1;  // toggles between 0 and 1 (LOW or HIGH), breaks for other numbers
-    digitalWrite( output_pin_B, toggle_me_B );
+//    toggle_me_B ^= 1;  // toggles between 0 and 1 (LOW or HIGH), breaks for other numbers
+    digitalWrite( output_pin_B, toggle_me_B^=1 );
     counter = 0;
   }
 }
 
-
+void test_while_loop()
+{
+  timer2_restart_zero();
+  counter++;
+  digitalWrite( output_pin_A, toggle_me_A^=1 );
+  volatile bool isFalse = false;
+  if ( isFalse ) while (true) { asm(""); }
+}
 
