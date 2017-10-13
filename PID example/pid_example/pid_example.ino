afdec5730c499e9e821dd6fcb088633a55f654f7
Tyler Lucas <tyblu@live.com>
2017-10-12 20:15:13 -0600

    Did a bunch of class tybluServo stuff that really needs to move to its own file.

--- PID example/pid_example/pid_example.ino
+++ PID example/pid_example/pid_example.ino
@@ -46,14 +46,18 @@ const double boomArmSensorOffset = -147.1303003827;
 
 void setup()
 {
-  boomArmServo.attach( boomArmPin );
-
   Serial.begin(9600);
 
   boomArmSensorValue = analogReadAverage( boomArmSensorPin, 100 );
   boomArmAngle = boomArmSensorSlope * boomArmSensorValue + boomArmSensorOffset;
-  boomArmAngle = constrain(boomArmAngle, boomArmLimits[0], boomArmLimits[1]);
+  while ( boomArmAngle < boomArmLimits[0] || boomArmAngle > boomArmLimits[1] )
+  {
+    boomArmSensorValue = analogReadAverage( boomArmSensorPin, 100 );
+    boomArmAngle = boomArmSensorSlope * boomArmSensorValue + boomArmSensorOffset;
+  }
+//  boomArmAngle = constrain(boomArmAngle, boomArmLimits[0], boomArmLimits[1]);
   boomArmServo.write(boomArmAngle);
+  boomArmServo.attach( boomArmPin );
 
   Serial.println("angle = slope * sensor value + offset");
   Serial.print( boomArmSensorSlope );
@@ -69,6 +73,8 @@ void setup()
 void loop()
 {
   int target = boomArmAngle + 15 * sin( millis() );
+//  if ( digitalRead(0) == HIGH )
+//    target = ( boomArmLimits[0] + boomArmLimits[1] ) / 2;
   target = constrain(target, boomArmLimits[0], boomArmLimits[1]);
   Serial.println(target); delay(100);
   int difference = target - boomArmAngle;
@@ -96,3 +102,140 @@ int analogReadAverage(int pin, int count)
     sum += analogRead( pin );
   return sum / count;
 }
+
+class tybluServo
+{
+public:
+  void setMinAngle(int);
+  void setMaxAngle(int);
+  void setServo(Servo);
+  void setSensorPin(int);
+  void setSensorSlope(double);
+  void setSensorOffset(double);
+
+  int getMinAngle();
+  int getMaxAngle();
+  int getAnalogAngle();
+  int getLastAngle();
+
+  void attach(int);
+  void detach();
+  void write(int);
+
+  tybluServo(Servo);
+  tybluServo(Servo, int, int);
+  tybluServo(Servo, int, int, int, double, double);
+
+private:
+  int minAngle, maxAngle;
+  Servo servo;
+  int sensorPin;
+  double sensorSlope, sensorOffset;
+
+  unsigned int analogValues[1024] = {0};  // for mode tallying
+};
+
+tybluServo::tybluServo(Servo servo)
+{
+  tybluServo.servo = servo;
+}
+
+tybluServo::tybluServo(Servo servo, int minAngle, int maxAngle)
+{
+  tybluServo.servo = servo;
+  tybluServo.minAngle = minAngle;
+  tybluServo.maxAngle = maxAngle;
+}
+
+tybluServo::tybluServo(Servo servo, int minAngle, int maxAngle, 
+  int sensorPin, double sensorSlope, double sensorOffset)
+{
+  tybluServo.servo = servo;
+  tybluServo.minAngle = minAngle;
+  tybluServo.maxAngle = maxAngle;
+  tybluServo.sensorSlope = sensorSlope;
+  tybluServo.sensorOffset = sensorOffset;
+}
+
+void tybluServo::setMinAngle(int minAngle)
+{
+  tybluServo.minAngle = minAngle;
+}
+
+void tybluServo::setMaxAngle(int maxAngle)
+{
+  tybluServo.maxAngle = maxAngle;
+}
+
+void tybluServo::setServo(Servo servo)
+{
+  tybluServo.servo = servo;
+}
+
+void tybluServo::setSensorPin(int sensorPin)
+{
+  tybluServo.sensorPin = sensorPin;
+}
+
+void tybluServo::setSensorSlope(double sensorSlope)
+{
+  tybluServo.sensorSlope = sensorSlope;
+}
+
+void tybluServo::setSensorOffset(double sensorOffset)
+{
+  tybluServo.sensorOffset = sensorOffset;
+}
+
+int tybluServo::getMinAngle()
+{
+  return tybluServo.minAngle;
+}
+
+int getMaxAngle()
+{
+  return tybluServo.maxAngle();
+}
+
+int getAnalogAngle()
+{
+  for (unsigned int i=0; i<1024; i++)
+    tybluServo.analogValues[i] = 0;
+  
+  for (unsigned int i=0; i<200; i++)
+    tybluServo.analogValues[ analogRead( tybluServo.sensorPin ) ] += 1;
+
+  int maxCount = 0;
+  int maxCountValue = 0;
+  for (unsigned int i=0; i<1024; i++)
+  {
+    if ( tybluServo.analogValues[i] > maxCount )
+    {
+      maxCount = analogValues[i];
+      maxCountValue = i;
+    }
+  }
+
+  return maxCountValue;
+}
+
+int tybluServo::getLastAngle()
+{
+  return tybluServo.servo.read();
+}
+
+void tybluServo::attach(int pin)
+{
+  tybluServo.servo.attach( pin );
+}
+
+void tybluServo::detach()
+{
+  tybluServo.servo.detach();
+}
+
+void tybluServo::write(int angle)
+{
+  tybluServo.servo.write( angle );
+}
+
