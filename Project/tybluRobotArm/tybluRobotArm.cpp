0feff396b505f529f931081eb96e2467c2b55394
Tyler Lucas <tyblu@live.com>
2017-10-14 15:47:07 -0600

    Back to a nearly blank slate.

--- Project/tybluRobotArm/tybluRobotArm.cpp
+++ Project/tybluRobotArm/tybluRobotArm.cpp
@@ -1,39 +1,16 @@
 #include <Arduino.h>
 #include "QuickStats.h"
+#include "TybluServo.h"
 
-void quickStatsDemo();
+TybluServo boomArmServo(75, 110, A0);
 
 void setup()
 {
 	Serial.begin(9600);
 	Serial.println("Compiled " __DATE__ " at " __TIME__);
-
-	Serial.println();
-	Serial.println();
-
-	quickStatsDemo();
 }
 
-void loop() {}
-
-void quickStatsDemo()
+void loop()
 {
-	Serial.println("QuickStats Demo");
 
-	float array[] = {1, 1, 2, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9, 100};
-	Serial.print("array[] = {");
-	for (int i=0; i<15; i++)
-	{
-		Serial.print(array[i]);
-		Serial.print(", ");
-	}
-	Serial.println("};");
-
-	QuickStats qs;
-	Serial.print("Average: ");
-	Serial.println( qs.average(array, 15) );
-	Serial.print("Geometric Average: ");
-	Serial.println( qs.g_average(array, 15) );
-	Serial.print("Mode: ");
-	Serial.println( qs.mode(array, 15, 0.1) );
 }
