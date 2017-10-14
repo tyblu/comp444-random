d06238043f1d2dbb8c22f8ebae96ea89e9a3d4b7
Tyler Lucas <tyblu@live.com>
2017-10-14 16:29:12 -0600

    Script to look at data coming from boom arm servo position sensor (wire). Gets 100 samples then calculates average, mode, and standard deviation with QuickStats. Works perfectly! Will set std. dev. limits with this info. TybluServo::getAnalogAngle() will return the mode only when the data set has a std. dev. below some amount... Maybe 1? 5? Will have to actually look at the data.

--- Project/tybluRobotArm/tybluRobotArm.cpp
+++ Project/tybluRobotArm/tybluRobotArm.cpp
@@ -3,14 +3,75 @@
 #include "TybluServo.h"
 
 TybluServo boomArmServo(75, 110, A0);
+int angle = (115+70)/2;
+long timestamp = millis();
+QuickStats qs;
+int angleAdjustment = 3;
 
 void setup()
 {
 	Serial.begin(9600);
 	Serial.println("Compiled " __DATE__ " at " __TIME__);
+	Serial.println();
+
+	boomArmServo.write(angle);
+	boomArmServo.attach(11);
+
+	delay(1000);
 }
 
 void loop()
 {
+	if (timestamp + 1000 > millis() )
+	{
+		long sampleTime = millis();
+		float array[100];
+		for (int i=0; i<100; i++)
+			array[i] = analogRead(A0);
+		sampleTime = millis() - sampleTime;
 
+		Serial.println();
+
+		Serial.print("Angle = ");
+		Serial.print( boomArmServo.read() );
+		Serial.print(", Sample Time = ");
+		Serial.print( sampleTime );
+		Serial.println(" ms");
+
+		Serial.print("Average = ");
+		sampleTime = millis();
+		float avg = qs.average(array, 100);
+		sampleTime = millis() - sampleTime;
+		Serial.print( avg );
+		Serial.print(", Compute Time = ");
+		Serial.print( sampleTime );
+		Serial.println(" ms");
+
+		Serial.print("  Mode =");
+		sampleTime = millis();
+		float mode = qs.mode(array, 100, 1);
+		sampleTime = millis() - sampleTime;
+		Serial.print( mode );
+		Serial.print(", Compute Time = ");
+		Serial.print( sampleTime );
+		Serial.println(" ms");
+
+		Serial.print("Std. Dev.=");
+		sampleTime = millis();
+		float stdev = qs.stdev(array, 100);
+		sampleTime = millis() - sampleTime;
+		Serial.print( stdev );
+		Serial.print(", Compute Time = ");
+		Serial.print( sampleTime );
+		Serial.println(" ms");
+	}
+	else
+	{
+		if (angle >= boomArmServo.getMaxAngle() || angle <= boomArmServo.getMinAngle() )
+			angleAdjustment *= -1;
+		angle += angleAdjustment;
+		boomArmServo.write(angle);
+		delay(1000);
+		timestamp = millis();
+	}
 }
