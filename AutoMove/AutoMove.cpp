c43e2274984f73995076eaad83606b4166572451
Tyler Lucas <tyblu@live.com>
2017-10-25 19:03:37 -0600

    Got rid of calibration stuff. Now just moves servos through their ranges. Works, mostly. Good enough.

--- AutoMove/AutoMove.cpp
+++ AutoMove/AutoMove.cpp
@@ -25,9 +25,9 @@
 TybluServo boom1Servo(BOOM1_PWM_PIN, 100, 150,  110, A1, 0.557, -49.64);	// TowerPro 946R, sensor needs verification
 TybluServo boom2Servo(BOOM2_PWM_PIN, 70, 115,  80, A0, 1.113, -147.1);	// Power HD 1501MG
 //TybluServo turretServo (TURRET_PWM_PIN, 30, 150,  90, A3, 1.000, -1.000);	// not measured
-//TybluServo clawServo (CLAW_PWM_PIN, 80, 130, 100, A2, 0.557, -61.38);	// TowerPro 946R, angles need verification
-#define NUM_ACTIVE_SERVOS 2
-TybluServo * servos[NUM_ACTIVE_SERVOS] = { &boom1Servo , &boom2Servo };
+TybluServo clawServo (CLAW_PWM_PIN, 60, 130, 100, A2, 0.557, -61.38);	// TowerPro 946R, angles need verification
+#define NUM_ACTIVE_SERVOS 1
+TybluServo * servos[NUM_ACTIVE_SERVOS] = { /*&boom1Servo , &boom2Servo,*/ &clawServo };
 
 void setup()
 {
@@ -50,17 +50,10 @@ void setup()
 			delay(10);
 		} while (!servos[i]->attached() );
 
-		DEBUG1("Calibrating Sensor...");
-
-		servos[i]->calibrateSensor();
-
-		DEBUG1(" Finished calibrating a servo. ");
-
-		servos[i]->smooth( servos[i]->getSafeAngle() );
-
-		Serial.println();
-		servos[i]->printSensorLine();
-		Serial.println(); delay(5);
+		servos[i]->smooth(servos[i]->getSafeAngle());
+		servos[i]->smooth(servos[i]->getMaxAngle());
+		servos[i]->smooth(servos[i]->getMinAngle());
+		servos[i]->smooth(servos[i]->getSafeAngle());
 
 		DEBUG1("Next servo (if any)...");
 		Serial.println();
